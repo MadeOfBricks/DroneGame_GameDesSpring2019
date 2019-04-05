31634e0ca82ad839e91dfe55c693dbbb800ccdfd
DESKTOP-3N49M5P\bclay <bclaymon@mavs.coloradomesa.edu>
2019-04-05 13:31:14 -0600

    updated BC

--- Source/DroneGame/ALiftTrigger.cpp
+++ Source/DroneGame/ALiftTrigger.cpp
@@ -21,15 +21,11 @@ void ALiftTriggerBox::OnOverlapBegin(class AActor* OverlappedActor, class AActor
 	if (OtherActor && (OtherActor != this)) {
 		UE_LOG(LogTemp, Warning, TEXT("Overlap begin"));
 		// not working. may try to move actor to top of trigger box when overlap begins
-		//FVector actorLocation = OverlappedActor->GetActorLocation();
 		FVector newLocation = OverlappedActor->GetActorLocation();
-		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Original Z value from ACTOR: %f"), actorLocation.Z));
 		newLocation.Z += 250000.0;
-		//SetActorLocation(newLocation, false);
-		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Z value from SET: %f"), newLocation.Z));
-		//FVector actorLocation = OverlappedActor->GetActorLocation();
-		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Z value from ACTOR: %f"), actorLocation.Z));
-		GetCharacterMovement()->AddImpulse(newLocation*100000, true);
+		SetActorLocation(newLocation, false);
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Z value from SET: %f"), newLocation.Z));
+		//GetCharacterMovement()->AddImpulse(newLocation*100000, true);
 	}
 }
 
