dfde05b7c443dab07971f97333ea8cb1d73bfbe1
DESKTOP-3N49M5P\bclay <bclaymon@mavs.coloradomesa.edu>
2019-04-08 17:51:38 -0600

    updated with bigger level

--- Source/DroneGame/AWinTrigger.cpp
+++ Source/DroneGame/AWinTrigger.cpp
@@ -26,7 +26,7 @@ void AWinTriggerBox::OnOverlapEnd(class AActor* OverlappedActor, class AActor* O
 {
 	if (OtherActor && (OtherActor != this)) {
 		UE_LOG(LogTemp, Warning, TEXT("Game over"));
-		//exits the editor entirely
+		// exits the editor entirely
 		FGenericPlatformMisc::RequestExit(false);
 	}
 }
